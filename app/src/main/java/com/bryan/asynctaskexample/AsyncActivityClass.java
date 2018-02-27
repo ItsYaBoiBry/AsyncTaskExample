0868da12e3c9b27c51155bf6f157a257f375a93a
Bryan <bryanlow987@gmail.com>
2018-02-27 11:52:02 +0800

    cleanup code.

--- app/src/main/java/com/bryan/asynctaskexample/AsyncActivityClass.java
+++ app/src/main/java/com/bryan/asynctaskexample/AsyncActivityClass.java
@@ -6,28 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class AsyncActivityClass extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_class);
-
         Intent intent = getIntent();
         String link = intent.getStringExtra("link");
         getJSONString results = new getJSONString();
         results.execute(link);
-
     }
 
     private class getJSONString extends AsyncTask<String, Void, String> {
-
         @Override
         protected String doInBackground(String... urls) {
             OkHttpClient client = new OkHttpClient();
@@ -51,7 +50,7 @@ public class AsyncActivityClass extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-            Log.i("executed result: ", result+"");
+            Log.i("executed result: ", result + "");
             Intent intent = new Intent();
             intent.putExtra("result", result);
             setResult(2, intent);
