bdb2f0bc64ba1c153693fbe2a1bffeb693886e4c
Bryan <bryanlow987@gmail.com>
2018-02-27 11:29:14 +0800

    Initial commit

---
+++ app/src/main/java/com/bryan/asynctaskexample/AsyncActivityClass.java
@@ -0,0 +1,61 @@
+package com.bryan.asynctaskexample;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class AsyncActivityClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_class);
+
+        Intent intent = getIntent();
+        String link = intent.getStringExtra("link");
+        getJSONString results = new getJSONString();
+        results.execute(link);
+
+    }
+
+    private class getJSONString extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(urls[0]).header("AccountKey", "SFtHKwbETzi/0lasguUW5g==").build();
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("ERROR : ", e.getLocalizedMessage());
+            }
+            if (response.isSuccessful()) {
+                try {
+                    return response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("executed result: ", result+"");
+            Intent intent = new Intent();
+            intent.putExtra("result", result);
+            setResult(2, intent);
+            finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/bryan/asynctaskexample/MainActivity.java
@@ -0,0 +1,73 @@
+package com.bryan.asynctaskexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int REQUEST_CODE = 2;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        textView = findViewById(R.id.jsonString);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AsyncActivityClass.class);
+                intent.putExtra("link", "http://datamall2.mytransport.sg/ltaodataservice/BusStops");
+                intent.putExtra("AccountKey","SFtHKwbETzi/0lasguUW5g==");
+                intent.putExtra("accept","application/json");
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==REQUEST_CODE){
+            String result = data.getStringExtra("result");
+            textView.setText(result);
+            Log.i("intent result: ", result +"");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
